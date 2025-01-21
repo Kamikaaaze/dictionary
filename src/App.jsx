343187f1e52f6d76d086ec6edf7125f81e3a25c4
aswin us <20br13512@rit.ac.in>
2025-01-21 22:52:41 +0530

    added useeffect,initialFetch and CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [userInput, setUserInput] = useState("");
+  const [userInput, setUserInput] = useState("Dictionary");
   const [keyword, setKeyword] = useState("Dictionary");
   const [phonetics, setPhonetics] = useState("/ˈdɪkʃəˌnɛɹi/");
   const [meaning, setMeaning] = useState([
@@ -13,10 +13,11 @@ function App() {
     " A synchronic dictionary of a sta…ain words that are properly part of the language.",
   ]);
   const [synonymn, setSynonymn] = useState(["wordbook"]);
-  const [verb, setVerb] = useState("sample example for verb statement");
-  const [nounEg,setNounEg] = useState("sample example here...")
+  const [verb, setVerb] = useState("To look up in a dictionary.");
+  const [nounEg, setNounEg] = useState("");
+  const [link, setLink] = useState("https://en.wiktionary.org/wiki/dictionary");
 
-  //phonetics fetching//
+  //API FETCHING //
   const search = async () => {
     try {
       const response = await axios.get(
@@ -24,7 +25,6 @@ function App() {
       );
       console.log(response.data);
       setPhonetics(response.data[0].phonetic);
-    
 
       if (response.data[0]?.meanings[0]?.definitions.length > 0) {
         setMeaning(
@@ -38,25 +38,77 @@ function App() {
         setSynonymn(response.data[0].meanings[0].synonyms);
       } else setSynonymn(["not available"]);
 
-      if (response.data[0]?.meanings[1]?.definitions.length > 0) {
+      if (response.data[2]?.meanings[0]?.definitions.length > 0) {
+        setVerb(response.data[2]?.meanings[0]?.definitions[0].definition);
+      } else if (response.data[0]?.meanings[1]?.definitions.length > 0) {
         setVerb(response.data[0]?.meanings[1]?.definitions[0].definition);
-      }else setVerb("not available");
+      } else setVerb("'not available' ");
 
-      // if(response.data[0]?.meanings[1]?.){
+      if (response.data[2]?.meanings[0]?.definitions.length > 0) {
+        setNounEg(response.data[2]?.meanings[0]?.definitions[0].example);
+      } else setNounEg("");
 
-      // }else setNounEg("not available");
+      if (response.data[0]?.sourceUrls) {
+        setLink(response.data[0]?.sourceUrls);
+      } else setLink("...");
     } catch (error) {
       console.log(error);
       setPhonetics("not available...");
       setMeaning(["not available"]);
       setSynonymn(["not available"]);
       setVerb("not available");
+      setNounEg("error");
     }
   };
   const keyWord = () => {
     setKeyword(userInput);
   };
 
+  useEffect(() => {
+    const initialResponse = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.dictionaryapi.dev/api/v2/entries/en/dictionary`
+        );
+        console.log(response.data);
+        setPhonetics(response.data[0].phonetic);
+
+        if (response.data[0]?.meanings[0]?.definitions.length > 0) {
+          setMeaning(
+            response.data[0]?.meanings[0]?.definitions.map(
+              (data) => data.definition
+            )
+          );
+        } else setMeaning("not available");
+
+        if (response.data[0]?.meanings[0]?.synonyms.length > 0) {
+          setSynonymn(response.data[0].meanings[0].synonyms);
+        } else setSynonymn(["not available"]);
+
+        if (response.data[2]?.meanings[0]?.definitions.length > 0) {
+          setVerb(response.data[2]?.meanings[0]?.definitions[0].definition);
+        } else if (response.data[0]?.meanings[1]?.definitions.length > 0) {
+          setVerb(response.data[0]?.meanings[1]?.definitions[0].definition);
+        } else setVerb("'not available' ");
+
+        if (response.data[2]?.meanings[0]?.definitions.length > 0) {
+          setNounEg(response.data[2]?.meanings[0]?.definitions[0].example);
+        } else setNounEg("");
+
+        if (response.data[0]?.sourceUrls) {
+          setLink(response.data[0]?.sourceUrls);
+        } else setLink("...");
+      } catch (error) {
+        console.log(error);
+        setPhonetics("not available...");
+        setMeaning(["not available"]);
+        setSynonymn(["not available"]);
+        setVerb("not available");
+        setNounEg("error");
+      }
+    };
+    initialResponse();
+  }, []);
   return (
     <div className="mainDiv">
       <div className="card">
@@ -66,10 +118,10 @@ function App() {
             onChange={(e) => {
               setUserInput(e.target.value);
             }}
-            placeholder="Type here.."
+           
             className="search-box"
             type="text"
-            defaultValue={"Dictionary"}
+            value={userInput}
           />
           <button
             onClick={() => {
@@ -114,7 +166,9 @@ function App() {
 
         <div className="source">
           <p>Source</p>
-          <p>www.link.com</p>
+          <a href={link} target="_blank" rel="no opener no referrer">
+            {link}
+          </a>
         </div>
       </div>
     </div>
